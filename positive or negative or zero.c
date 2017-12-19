cb0b7e615d1d2986ac164cadf587435ea4b032de
poojasriravichandran <34672021+poojasriravichandran@users.noreply.github.com>
2017-12-19 11:45:11 +0530

    Create positive or negative or zero.c

---
+++ positive or negative or zero.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int num;
+	scanf("%d",&num);
+	if(num==0)
+	printf("ZERO");
+	else if(num>0)
+	printf("POSITIVE");
+	else 
+	printf("NEGATIVE");
+	return 0;
+}
